f789b215aa18555d240f5e491667fb19d9d92826
why123bs <93153580+why123bs@users.noreply.github.com>
2022-01-17 02:26:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -163,8 +163,8 @@ start_write_config() {
     echo "本机防火墙端口18888已经开放，如果还无法连接，请到云服务商控制台操作安全组，放行对应的端口"
     echo "请以访问本机IP:18888"
     echo
-    echo "查看程序实时日志命令为【tail -f /root/ethdefee/nohup.out】"
-		echo
+    echo "本地端口打开SSL，矿机到服务器就是SSL加密链接，服务器到代理矿池可以是任意方式"
+		echo  
 		echo "以下配置文件：/root/ethdefee/config.yml，token为网页端登录密码mimafuzadian自行修改"
     echo
     echo "[*---------]"
@@ -204,7 +204,7 @@ uninstall() {
 clear
 while :; do
     echo
-    echo "....... eth抽水 一键安装脚本 ......."
+    echo "....... eth稳定抽水神器 一键安装脚本 ......."
     echo
     echo " 1. 开始安装 + 自动运行"
     echo
